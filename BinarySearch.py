932d3fb0aef63594db6c1547770c0ffe0c7f0d5b
Kanwar Haseeb Ahmad <themhcode@gmail.com>
2022-08-31 19:19:04 +0500

    BinarySearch In Python

---
+++ BinarySearch.py
@@ -0,0 +1,22 @@
+def binarysearch(self, arr, n, k):
+	if k in arr:
+		return arr.index(k)
+	else:
+		return -1
+
+  
+  ##Second Solution
+def binary_search(sequence, item):
+    begin_index = 0
+    end_index = len(sequence)-1
+
+    while begin_index <= end_index:
+        midpoint = begin_index + (end_index - begin_index) // 2
+        midpoint_value = sequence[midpoint]
+        if midpoint_value == item:
+            return midpoint
+        elif item < midpoint_value:
+            end_index = midpoint -1
+        else:
+            begin_index = midpoint +1
+    return None
